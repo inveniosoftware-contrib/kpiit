9a074c4c4d8cc4036e67a7c083cd6fc75bf994ec
Niklas Persson <equadon@gmail.com>
2018-06-26 16:46:55 +0200

    Add provider for records by URL

--- kpiit/models.py
+++ kpiit/models.py
@@ -12,39 +12,44 @@ import requests
 
 class Metric(object):
     def __init__(self, name):
-        pass
+        self.name = name
 
-    def update(self, data):
+    def update(self, *args, **kwargs):
         raise NotImplementedError()
 
 
 class MetricInstance(object):
-    def __init__(self, provider, metric):
+    def __init__(self, provider):
         self.provider = provider
-        self.metric = metric
+        self.metric = provider.metric
+
+    def collect(self):
+        return self.provider.collect()
 
 
 class Provider(object):
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, metric):
+        self.metric = metric
 
     def collect(self):
         raise NotImplementedError()
 
 
 class FileProvider(Provider):
-    def __init__(self, filename):
+    def __init__(self, metric, filename):
+        super().__init__(metric)
         self.filename = filename
 
 
 class URLProvider(Provider):
-    def __init__(self, url):
+    def __init__(self, metric, url):
+        super().__init__(metric)
         self.url = url
 
 
 class RecordsMetric(Metric):
     def __init__(self):
-        super('num_records')
+        super().__init__('num_records')
 
     def update(self, count):
         self.__count = count
@@ -54,12 +59,25 @@ class RecordsMetric(Metric):
         return self.__count
 
 
-class ZenodoRecordsProvider(URLProvider):
-    def __init__(self):
-        super('zenodo', RecordsMetric())
+class RecordsURLProvider(URLProvider):
+    """Provider that retrieves records data from a URL."""
+
+    def __init__(self, url):
+        super().__init__(RecordsMetric(), url)
 
     def collect(self):
-        """Collect num_records from Zenodo."""
-        req = requests.get(url)
+        """Collect num_records from given URL."""
+        req = requests.get(self.url)
         data = req.json()
         record_count = data['hits']['total']
+
+        self.metric.update(record_count)
+
+        return record_count
+
+
+class ZenodoRecordsMetric(MetricInstance):
+    ZENODO_RECORDS_URL = 'https://zenodo.org/api/records/?all_versions'
+
+    def __init__(self):
+        super().__init__(RecordsURLProvider(ZenodoRecordsMetric.ZENODO_RECORDS_URL))
