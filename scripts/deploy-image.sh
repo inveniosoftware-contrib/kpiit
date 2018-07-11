061a720c7bdeb375ea79cef6159cfdbbfc3a8d62
Niklas Persson <equadon@gmail.com>
2018-07-11 15:47:10 +0200

    Update deploy image script with authentication

--- scripts/deploy-image.sh
+++ scripts/deploy-image.sh
@@ -7,6 +7,20 @@ if [ -z "$VERSION" ]; then
     exit
 fi
 
+# Ensure the user is authenticated with oc
+who_ret=$(oc whoami)
+if [ -z "$who_ret" ]; then
+    echo "Error: Please login with oc login before running the script."
+    exit 1
+fi
+
+# Login to Docker
+docker_login_ret=`docker login -u openshift -p $(oc whoami -t) openshift-registry.web.cern.ch`
+if [ "$docker_login_ret" != "Login Succeeded" ]; then
+    echo "Error: Failed to login to Docker."
+    exit 1
+fi
+
 BUILD="docker build --no-cache --network=host -t kpiit-image:${VERSION} ."
 echo "Building image: $BUILD"
 $BUILD
